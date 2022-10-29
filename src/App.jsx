80f8522000c3636b1edf71b80e872afc3096af7b
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2022-10-29 13:27:20 +0100

    Added share icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import Footer from './Footer';
 import ListItem from './ListItem';
-import profile from './assets/profile__img.png';
+import share_ from './assets/share.png';
+import shareHover from './assets/share-hover.png';
+import profile_img from './assets/profile__img.png';
+import profile_alt from './assets/profile_alt.png';
+import slack from './assets/slack.png';
+import github from './assets/github.png';
 
 const lists = [
   {name: "Twitter Link", link: "https://twitter.com/EvuetaphaJoshua/", id: "twitterLink"},
@@ -13,20 +19,47 @@ const lists = [
 
 
 function App() {
+
+  const [profile, setProfile] = useState(profile_img);
+  const [share, setShare] = useState(share_);
+  const [shareClass, setShareClass] = useState('share');
+
+  const shareHoverFunc = () => {
+    setShare(shareHover)
+    setShareClass("share-hover")
+  }
+
+  const shareFunc = () => {
+    setShare(share_)
+  }
+
   return (
     <div>
 
       <div className="app">
 
-        <img className='profile' alt='profile' id="profile__img" src={profile} />
+        <img onMouseOver={shareHoverFunc} onMouseOut={shareFunc} className={shareClass} alt='share' id="share" src={share} />
 
-        <h2 className='name' id="twitter">EvuetaphaJoshua</h2>
+        <div className='profile-section'>
 
-        <h2 id="slack" style={{display: "none"}}> joshuajee </h2>
+          <img onMouseOver={() => setProfile(profile_alt)} onMouseOut={() => setProfile(profile_img)} className='profile' alt='profile' id="profile__img" src={profile} />
+
+          <h2 className='name' id="twitter">EvuetaphaJoshua</h2>
+
+          <h2 id="slack" style={{display: "none"}}> joshuajee </h2>
+        </div>
 
         {
           lists.map((list, index) => <ListItem key={index} name={list.name} link={list.link} id={list.id} />)
         }
+
+        <div className="socials-container">
+
+          <img alt={"slack"} src={slack} />
+
+          <img alt={"github"} src={github} />
+
+        </div>
 
       </div>
 
