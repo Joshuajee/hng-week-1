909701bf9b8fb1ed17492192c9a458b765308708
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2022-11-05 14:09:51 +0100

    feat: added outline to input field

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,18 +10,29 @@ export default function Contact () {
     const [emailHint, setEmailHint] = useState(false)
     const [messageHint, setMessageHint] = useState(false)
 
+    const error = (value) => {
+        return value ? "error" : "" 
+    }
+
+    const validateTextField = (value, length) => {
+        if (value.length <= length) return true
+
+        return false
+    }
 
     const editFirstName = (e) => {
         const value = e.target.value
         setFirstName(value)
-        if (value.length <= 2) return setFirstNameHint(true)
+
+        if (validateTextField(value , 2)) return setFirstNameHint(true)
         setFirstNameHint(false)
     }
 
     const editLastName = (e) => {
         const value = e.target.value
         setLastName(value)
-        if (value.length <= 2) return setLastNameHint(true)
+
+        if (validateTextField(value, 2)) return setLastNameHint(true)
         setLastNameHint(false)
     }
 
@@ -41,6 +52,7 @@ export default function Contact () {
         e.preventDefault()
     } 
 
+
     return (
         <div className="contact__form">
 
@@ -54,13 +66,13 @@ export default function Contact () {
 
                     <div className="field">
                         <label>First Name</label>
-                        <input type={"text"} required id="first_name" placeholder="Enter your first name" onChange={editFirstName} />
+                        <input className={error(firstNameHint)} type={"text"}  id="first_name" placeholder="Enter your first name" onChange={editFirstName} />
                         {firstNameHint && <text className="helper-text">Name too short, minumum of 3 characters</text>}
                     </div>
 
                     <div className="field">
                         <label>Last Name</label>
-                        <input type={"text"} required id="last_name" placeholder="Enter your last name" onChange={editLastName} />
+                        <input className={error(lastNameHint)}  type={"text"}  id="last_name" placeholder="Enter your last name" onChange={editLastName} />
                         {lastNameHint && <text className="helper-text">Name too short, minumum of 3 characters</text>}
                     </div>
 
@@ -68,18 +80,18 @@ export default function Contact () {
 
                 <div>
                     <label>Email</label>
-                    <input type={"email"} required id="email" placeholder="yourname@email.com" onChange={editEmail} />
+                    <input className={error(emailHint)}  type={"email"} id="email" placeholder="yourname@email.com" onChange={editEmail} />
                     { emailHint && <text className="helper-text">Invalid email address.</text> }
                 </div>
 
                 <div className="msg_container">
                     <label>Message</label>
-                    <textarea id={"message"} required placeholder="Send me a message i will reply as soon as possible..." onChange={editMessage}></textarea>
+                    <textarea className={error(messageHint)}  id={"message"} placeholder="Send me a message i will reply as soon as possible..." onChange={editMessage}></textarea>
                     {messageHint && <text className="helper-text" style={{color: "#F83F23"}}>Message too short, minumum of 10 characters</text>}
                 </div>
 
                 <div className="term">
-                    <input type={"checkbox"} required className="term_check" />
+                    <input type={"checkbox"}  className="term_check" />
                     <div> <text>You agree to providing your data to <strong>{firstName} {lastName}</strong> who may contact you.</text></div>
                     {/* <div><text className="helper-text">This is a hint text to help user.</text></div> */}
                 </div>
